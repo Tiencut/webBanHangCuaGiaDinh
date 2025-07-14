4d07838774bb2721b769a63bc638a4b23e4a3f3e
Tiencut <tiencut@gmail.com>
2025-07-14 20:28:25 +0700

    xoa 2 foler FE

--- frontend-web/src/api/index.js
+++
@@ -1,6 +0,0 @@
-// Export all API services
-export { productsAPI } from './products.js';
-export { customersAPI } from './customers.js';
-export { vehiclesAPI } from './vehicles.js';
-export { ordersAPI } from './orders.js';
-export { default as api } from './config.js'; 
